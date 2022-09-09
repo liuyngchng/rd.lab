5061f6eee508c415813fde219c1077f4bbb49235
rd <liuyongcheng01@cnpc.com.cn>
2022-09-09 09:47:14 +0800

    debug

--- shell/get_time.sh
+++ shell/get_time.sh
@@ -1,4 +1,6 @@
 # 10#表示将后面的数字作为10进制处理，否则，当出现0xxx时，
 # shell会认为是8进制，会报错
+# display milisecond
 #echo -n $(date '+%Y-%m-%d %H:%M:%S').$((10#`date '+%N'`/1000000))
+# nanosecond
 echo $(date '+%Y-%m-%d %H:%M:%S').$(date '+%N')
