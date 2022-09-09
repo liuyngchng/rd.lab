026513cc4983ffbecafc02934122addd9cfe0bf7
rd <liuyongcheng01@cnpc.com.cn>
2022-09-09 10:41:20 +0800

    debug

--- shell/clock.sync/sync_test.sh
+++ shell/clock.sync/sync_test.sh
@@ -2,7 +2,7 @@ echo '#### start ####'
 echo "local  time: $(date '+%Y-%m-%d %H:%M:%S').$(date '+%N')"
 #echo $(date '+%Y-%m-%d %H:%M:%S').$((`date '+%N'`/1000000))
 #echo -e '\nstart get server time'
-echo "server time: $(curl -I http://www.baidu.com)"
+echo "server time: $(curl -I --noproxy '*' http://www.baidu.com)"
 echo "local  time: $(date '+%Y-%m-%d %H:%M:%S').$(date '+%N')"
 echo "local  time: $(date '+%Y-%m-%d %H:%M:%S').$(date '+%N')"
 #echo $(date '+%Y-%m-%d %H:%M:%S').$((`date '+%N'`/1000000))
