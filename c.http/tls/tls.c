fe09efcbc345495b91fa73225c9c57604b3690aa
richard <liuyngchng@hotmail.com>
2023-12-09 15:25:39 +0800

    debug

--- c.http/tls/tls.c
+++ c.http/tls/tls.c
@@ -4,14 +4,16 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
 #include <unistd.h>
-#define MAXBUF 1024
+#define MAXBUF 8096
 #define _PORT_ 8899
 
 int main(){
     SSL_CTX *ctx;
-    int sockfd, cfd;
-    socklen_t clilen;
+    int sfd, cfd;
+    socklen_t len;
+    int sockopt = 1;
     struct sockaddr_in serv_addr, cli_addr;
     char buffer[MAXBUF];
     int n;
@@ -35,7 +37,7 @@ int main(){
         exit(1);
     }
     //创建套接字并绑定端口
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("创建socket失败");
         exit(1);
     }
@@ -43,43 +45,49 @@ int main(){
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(_PORT_);
-    int i=bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(int));
+    int i=bind(sfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     if (i < 0) {
-        printf("bind failed, port %d", _PORT_);
+        printf("bind failed, port %d, %s\n", _PORT_, strerror(errno));
         exit(1);
     }
-    if (listen(sockfd, 10) < 0) {
+    if (listen(sfd, 10) < 0) {
     	printf("listen failed\n");
         exit(1);
     }
     printf("listening port %d\n", _PORT_);
-    clilen = sizeof(cli_addr);
-    cfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-    if (cfd < 0) {
-        printf("accept failed\n");
-        exit(1);
+    len = sizeof(cli_addr);
+    while (1) {
+		cfd = accept(sfd, (struct sockaddr *)&cli_addr, &len);
+		if (cfd < 0) {
+			printf("accept failed, %s\n", strerror(errno));
+			exit(1);
+		}
+		char *ip = inet_ntoa(cli_addr.sin_addr);
+		printf("%s connected\n", ip);
+		ssl = SSL_new(ctx);
+		SSL_set_fd(ssl, cfd);
+		if (SSL_accept(ssl) == -1) {
+			ERR_print_errors_fp(stderr);
+			printf("ssl accept failed, %s\n", strerror(errno));
+		} else {
+			printf("TLS connection established.\n");
+			//snd buf
+			char *msg="HTTP/1.1 200 OK\r\n"
+				"Content-Type: application/json\r\n\r\n"
+				"{\"status\":200}";
+			SSL_write(ssl, msg, strlen(msg));
+			printf("send msg %s\n", msg);
+			//rcv buf
+			bzero(buffer, MAXBUF);
+			SSL_read(ssl, buffer, MAXBUF);
+			printf("received msg: %s\n", buffer);
+		}
+		SSL_shutdown(ssl);
+		SSL_free(ssl);
+		close(cfd);
     }
-    printf("connected\n");
-    ssl = SSL_new(ctx);
-    SSL_set_fd(ssl, cfd);
-    if (SSL_accept(ssl) == -1) {
-        ERR_print_errors_fp(stderr);
-        printf("ssl accept failed\n");
-    } else {
-    	printf("TLS connection established.\n");
-        //snd buf
-    	char *msg="hello, TLS world";
-        SSL_write(ssl, msg, strlen(msg));
-        printf("send msg %s\n", msg);
-        //rcv buf
-        bzero(buffer, MAXBUF);
-        SSL_read(ssl, buffer, MAXBUF);
-        printf("received msg: %s\n", buffer);
-    }
-    SSL_shutdown(ssl);
-    SSL_free(ssl);
-    close(cfd);
-    close(sockfd);
+    close(sfd);
     SSL_CTX_free(ctx);
     return 0;
 }
