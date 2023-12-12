3aba4ee64a1751854b99db3408dca43e2eb6968e
rd <whoami@hotmail.com>
2023-12-12 10:38:10 +0800

    debug

--- c.http/deploy.sh
+++ c.http/deploy.sh
@@ -1,4 +1,21 @@
 app=srvr
+
+function check_log() {
+  my_app=$1
+  trigger='srv_started'
+#  sed /"${trigger}"/q; kill $!;  < <(exec timeout 1m docker logs -f ${my_app})
+    docker logs -f ${my_app} | while read line
+    do
+      echo $line
+        if echo $line | grep "${trigger}" --color;then
+          echo -e "\e[1;32mapp:${my_app} deploy OK\e[0m"
+          kill $$
+          break
+        fi
+    done
+    return 0
+}
+
 old_img=`docker images | grep '<none>' | awk -F ' ' '{print$3}'`
 cmd='make clean'
 echo -e "\e[1;32m${cmd}\e[0m"
@@ -26,6 +43,6 @@ eval ${cmd}
 cmd="docker run -dit --name ${app} --rm -p 8083:8083 ${app}"
 echo -e "\e[1;32m${cmd}\e[0m"
 eval ${cmd}
-cmd="docker logs -f ${app}"
+cmd="check_log ${app}"
 echo -e "\e[1;32m${cmd}\e[0m"
 eval ${cmd}
